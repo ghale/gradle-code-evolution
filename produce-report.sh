de6fe9638e8906848217432dd0ea726ca064b933
Lóránt Pintér <lorant@gradle.com>
2024-05-14 12:23:56 +0200

    Update

--- produce-report.sh
+++ produce-report.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+output_file="${1:-report.tsv}"
+
 if [ ! -d gradle ]; then
-    git clone https://github.com/gradle/gradle.git
+    git clone -q https://github.com/gradle/gradle.git
+else
+    git -C gradle fetch --all -q
 fi
 
 git="git -C gradle"
@@ -13,21 +17,57 @@ version_greater() {
 }
 
 versions=""
-base_tag="v7.0.0"
+base_tag="v8.0.0"
 for tag in $($git tag -l 'v[0-9]*\.[0-9]*\.0'); do
     if version_greater $tag $base_tag; then
         versions="$versions $tag"
     fi
 done
 
-#versions="$base_tag $versions release master"
-versions="master"
+versions="$base_tag $versions release master"
+#versions="master"
+
+get_child_directories() {
+  local root_dir=$1
+  local depth=$2
+
+  if [ -d "$root_dir" ]; then
+    find "$root_dir" -mindepth "$depth" -maxdepth "$depth" -type d
+  else
+    echo ""
+  fi
+}
 
 process_version() {
-    echo "Processing: $1"
-    $git checkout $1
-    echo "Commits: $($git rev-list --count $1)"
+    tag=$1
+    version=${tag#v}
+    echo "Processing: $version"
+
+    commit_count=$($git rev-list --count $tag)
+    printf "Commits\t%s\t%d\n" $version $commit_count >> $output_file
+
+    $git checkout -q $tag
+    $git clean -qfdx
+
+    # Find all subprojects
+
+    old_subprojects_dirs=$(get_child_directories gradle/subprojects 1)
+    platform_dirs=$(get_child_directories gradle/platforms 2)
+    testing_dirs=$(get_child_directories gradle/testing 1)
+
+    subproject_dirs="$old_subprojects_dirs $platform_dirs $testing_dirs"
+
+    for subproject_dir in $subproject_dirs; do
+        subproject=":$(basename $subproject_dir)"
+        if [ ! -d "$subproject_dir/src/main" ]; then
+            continue
+        fi
+        total_lines=$(find "$subproject_dir/src/main" -type f -print0 | xargs -0 cat | wc -l)
+        printf "%s\t%s\t%d\n" "$subproject" "$version" "$total_lines" >> $output_file
+    done
 }
+
+printf "Subproject\tVersion\tLOC\n" > $output_file
 
 for version in $versions; do
     process_version $version
