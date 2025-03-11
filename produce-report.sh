c55b06aa5fb3d5174edf8cd008b01c87bf15a118
Gary Hale <gary@gradle.com>
2025-03-11 13:07:08 -0400

    Adapt for measuring files in non-platformized projects

--- produce-report.sh
+++ produce-report.sh
@@ -2,13 +2,16 @@
 setopt null_glob
 
 output_file="${1:-report.tsv}"
-base_tag="v0.7.0"
+base_date="2023-01-01"
 
 if [ ! -d gradle ]; then
+    echo "Cloning repository and checking out master"
     git clone -q https://github.com/gradle/gradle.git
+    git -C gradle checkout -q master
 else
+    echo "Resetting repository to current master"
     git -C gradle fetch --all -q
-    git -C gradle reset --hard -q
+    git -C gradle reset --hard -q origin/master
     git -C gradle clean -qfdx
 fi
 
@@ -16,39 +19,18 @@ git_cmd() {
     git -C gradle "$@"
 }
 
-# Function to compare versions
-version_greater() {
-    # Remove 'v' from tags and compare as integers
-    [ "$(echo "$1" | tr -d 'v' | awk -F. '{ printf("%d%03d%03d\n", $1,$2,$3); }')" -gt "$(echo "$2" | tr -d 'v' | awk -F. '{ printf("%d%03d%03d\n", $1,$2,$3); }')" ]
+getCommitFromOneWeekBefore() {
+    commit=$1
+    git_cmd log -1 --merges --since="$base_date" --before="$( date -j -f "%Y-%m-%d" -v-1w "+%Y-%m-%d" $(git_cmd log -1 --format="%cd" --date=short ${commit}))" --format="%H"
 }
 
-# Function to convert a semantic version string to a sortable string
-sortable_version() {
-    local version=$1
-    # Remove the 'v' prefix
-    version=${version#v}
-    # Pad each part of the version with leading zeros to ensure proper sorting
-    echo $version | awk -F. '{ printf("%03d.%03d.%03d\n", $1, $2, $3); }'
-}
-
-typeset -A sorted_versions
-for tag in $(git_cmd tag -l 'v[0-9]*\.[0-9]*\.0'); do
-    if ! version_greater $base_tag $tag; then
-        sortable=$(sortable_version $tag)
-        sorted_versions[$sortable]=$tag
-    fi
+echo "Finding weekly commits going back to $base_date"
+next_commit=$(git_cmd log -1 --format="%H")
+all_commits=($next_commit)
+while [[ "$next_commit" != "" ]] ; do
+    next_commit=$(getCommitFromOneWeekBefore $next_commit)
+    all_commits+=($next_commit)
 done
-
-# Sort the keys of the associative array
-sorted_keys=(${(On)${(k)sorted_versions}})
-
-versions=()
-for key in $sorted_keys; do
-    versions=(${sorted_versions[$key]} ${versions[@]})
-done
-
-versions+=("release" "master")
-#versions+="master"
 
 get_child_directories() {
     local root_dir=$1
@@ -64,104 +46,48 @@ get_child_directories() {
     echo "${directories[@]}"
 }
 
-is_in_tags() {
-    local tag=$1
-    local tags_list=$2
-
-    for t in $tags_list; do
-        if [[ "$t" == "$tag" ]]; then
-            return 0
-        fi
-    done
-    return 1
-}
-
-count_lines_of_code() {
+count_files() {
   local target=$1
 
   # Run cloc and extract the total lines of code from its output
-  total_lines=$(cloc --json "$target" | jq '.SUM.code')
-  echo "$total_lines"
+  total_files=$(find $target -type f | wc -l)
+  echo "$total_files"
 }
 
 process_version() {
-    tag=$1
-    versions=$2
+    commit=$1
 
     # commit_count=$(git_cmd rev-list --count $tag)
-    # printf "Commits\t%s\t%d\n" $version $commit_count >> $output_file
+    # printf "Commits\t%s\t%d\n" commit $commit_count >> $output_file
 
-    git_cmd checkout -q $tag
+    git_cmd checkout -q $commit
     git_cmd clean -qfdx
-
-    if [[ "$tag" == v* ]]; then
-        version=${tag#v}
-    else
-        gradle_version=$(cat gradle/version.txt)
-        if [[ "$gradle_version" =~ ^[0-9]+\.[0-9]+$ ]]; then
-            if is_in_tags "v$gradle_version.0" "$versions"; then
-                echo "Skipping tag '$tag' with version '$gradle_version' as it has already been processed"
-                return
-            fi
-            version="$gradle_version.0 ($tag)"
-        else
-            echo "Skipping tag '$tag' with version '$gradle_version' as it points to a patch release"
-            return
-        fi
-    fi
 
     # Find all subprojects
 
-    old_subprojects_dirs=($(get_child_directories gradle/subprojects 1))
-    platform_dirs=($(get_child_directories gradle/platforms 2))
-    testing_dirs=($(get_child_directories gradle/testing 1))
+    all_subproject_dirs=($(get_child_directories gradle/subprojects 1))
 
-    all_subproject_dirs=(gradle ${old_subprojects_dirs[@]} ${platform_dirs[@]} ${testing_dirs[@]})
-
-    buildSrc_subproject_dirs=($(get_child_directories gradle/buildSrc/subprojects 1))
-    build_logic_dirs=($(get_child_directories gradle/build-logic 1))
-    build_logic_commons_dirs=($(get_child_directories gradle/build-logic-commons 1))
-    build_logic_settings_dirs=($(get_child_directories gradle/build-logic-settings 1))
-
-    all_build_logic_dirs=(gradle/buildSrc ${buildSrc_subproject_dirs[@]} ${build_logic_dirs[@]} ${build_logic_commons_dirs[@]} ${build_logic_settings_dirs[@]})
-
-    all_dirs=(${all_subproject_dirs[@]} ${all_build_logic_dirs[@]})
-
-    echo "Processing: $version with ${#all_subproject_dirs} subprojects and ${#all_build_logic_dirs} build logic dirs"
+    date=$(git_cmd show -s --format="%cs" $commit)
+    echo "Processing: $commit from $date with ${#all_subproject_dirs} subprojects"
 
     for subproject_dir in $all_subproject_dirs; do
         if [ ! -d "$subproject_dir/src/main" ]; then
             continue
         fi
         subproject=":$(basename $subproject_dir)"
-        total_lines=$(count_lines_of_code "$subproject_dir/src/main")
-        printf "%s\t%s\t%d\n" "$subproject" "$version" "$total_lines" >>$output_file
+        total_files=$(count_files "$subproject_dir/src/main")
+        printf "%s\t%s\t%d\n" "$subproject" "$date" "$total_files" >>$output_file
     done
-
-    for build_logic_dir in $all_build_logic_dirs; do
-        if [ ! -d "$build_logic_dir/src/main" ]; then
-            continue
-        fi
-        subproject=":$(basename $build_logic_dir)"
-        total_lines=$(count_lines_of_code "$build_logic_dir/src/main")
-        printf "%s\t%s\t-%d\n" "$subproject" "$version" "$total_lines" >>$output_file
-    done
-
-    total_build_script_lines=0
-    for dir in $all_dirs; do
-        build_files=($dir/*.gradle $dir/*.gradle.kts)
-        for build_file in $build_files; do
-            if [ -f "$build_file" ]; then
-                lines=$(count_lines_of_code "$build_file")
-                (( total_build_script_lines += lines ))
-            fi
-        done
-    done
-    printf "Build scripts\t%s\t-%d\n" "$version" "$total_build_script_lines" >>$output_file
 }
 
-printf "Subproject\tVersion\tLOC\n" >$output_file
+printf "Subproject\tDate\tFiles\n" >$output_file
 
-for version in $versions; do
-    process_version $version $versions
+reversed_commits=()
+for (( i=${#all_commits[@]} ; i>=0 ; i-- )); do
+    reversed_commits+=(${all_commits[$i]})
+done
+
+for commit in ${reversed_commits}; do
+    #git_cmd log -1 ${commit}
+    process_version $commit
 done
